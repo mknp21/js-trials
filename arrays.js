64b360d9240b43fc77faf09149bc2bef8b5acded
Mika Patterson <mknpatt21@gmail.com>
2021-09-21 01:46:41 +0000

    Working on printIndices

---
+++ arrays.js
@@ -0,0 +1,40 @@
+"use strict";
+
+
+// 1. printIndices
+const printIndices = items => {
+  for (const item of items) {
+    // const stuff = `${item} ${items}`;
+    const fruit = item;
+    // console.log(stuff);
+  }
+  for (const i in items) {
+    const num = i;
+  }
+  printIt = `${fruit} ${num}`
+  console.log(printIt);
+};
+
+// or… of… gets you each element 
+// // Loop over elements
+// for (const state of states) {
+//   console.log(state);
+// }
+
+// for… in… gets you each index
+
+// // Loop over indices
+// for (const i in states) {
+//   console.log(i);
+// }
+
+// 2. everyOtherItem
+// function everyOtherItem(items) {
+  // Replace this with your code
+// }
+
+
+// 3. smallestNItems
+// function smallestNItems(items, n) {
+  // Replace this with your code
+// }
